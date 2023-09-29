9888fbcf4b248900fd204db02b251bf2136b467c
kappaprideonly <kolmanoney@gmail.com>
2023-09-29 23:31:48 +0300

    graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"url_shortener/config"
 	httpApi "url_shortener/internal/controller/http"
 	"url_shortener/internal/repository"
 	"url_shortener/internal/service"
 	"url_shortener/pkg/db/postgres"
 	"url_shortener/pkg/logger/zerolog"
+	"url_shortener/pkg/server"
 
 	"github.com/heetch/confita"
 	"github.com/heetch/confita/backend/env"
@@ -33,9 +36,19 @@ func main() {
 	repo := repository.NewRepository(db, logger)
 	service := service.NewService(logger, repo.GetLinkRepository())
 	logger.Info(fmt.Sprint(cfg))
-	server := httpApi.SetupRouter(logger, service)
-	err = http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), server)
-	if err != nil {
-		logger.Fatal(err.Error())
+	router := httpApi.SetupRouter(logger, service)
+	srv := new(server.Server)
+	go func() {
+		if err := srv.Run(cfg.Port, router); err != nil {
+			logger.Fatal(err.Error())
+		}
+	}()
+	logger.Info("app started")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	logger.Info("app shutting down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logger.Error(err.Error())
 	}
 }
